e207aa07d0927f0ee0c0349f155d7497d02d02cf
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-03-18 13:18:45 -0400

    Add testing for iterative function, recursive function (still need to write), and edge cases.

---
+++ tests/fibonacci.test.js
@@ -0,0 +1,21 @@
+import { fibs, fibsRec } from "../src/fibonacci";
+
+// Description, then function to check
+// For iterative function:
+test("iterative fibonacci returns correct array", () => {
+  expect(fibs(8)).toEqual([0, 1, 1, 2, 3, 5, 8, 13]);
+});
+
+// For recursive function:
+test("recursive fibonacci returns correct array", () => {
+  expect(fibsRec(8)).toEqual([0, 1, 1, 2, 3, 5, 8, 13]);
+});
+
+// Testing for edge cases (small input)
+(test("handles small input"),
+  () => {
+    expect(fibs(1)).toEqual([0]);
+    expect(fibsRec(1)).toEqual([0]);
+    expect(fibs(0)).toEqual([]);
+    expect(fibsRec(0)).toEqual([]);
+  });
